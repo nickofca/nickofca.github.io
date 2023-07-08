caf677168d0eb4a0dd0e23ce29cc8e200d5c24ae
nickofca <nickfzca@gmail.com>
2023-07-07 21:30:43 -0600

    Completed full list of pages needed for basic profile including Home, About, Blog, and Contact. Basic navigation on an app-wide level with MVP styling included.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Blog from './components/Blog';
-import Home from './components/Home'
+import Home from './components/Home';
+import About from './components/About';
+import styles from './App.module.css';  // Import the CSS module
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/blog" element={<Blog />} />
-      </Routes>
+      <div className={styles.container}>
+        <nav>
+          <ul className={styles.nav}>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/blog">Blog</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,13 +1,29 @@
-// src/components/Blog.js
 import React from 'react';
+import Article from './Article';
 
-function Blog() {
-  return (
-    <div>
-      <h1>Blog</h1>
-      <p>Welcome to the blog page!</p>
-    </div>
-  );
+class Blog extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      articles: [
+        // This data could also come from an API or a database
+          // TODO: Implement a database to store articles
+        { title: 'Article 1', date: '2023-07-01', author: 'Author 1', content: 'Content 1' },
+        { title: 'Article 2', date: '2023-07-02', author: 'Author 2', content: 'Content 2' },
+        // Add as many articles as you want here
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.articles.map((article, index) =>
+          <Article key={index} data={article} />
+        )}
+      </div>
+    );
+  }
 }
 
 export default Blog;
